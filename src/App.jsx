312977a9bc62bd5da82a6bda5c66651314df9c61
LCuretti <curetti.luis@gmail.com>
2023-11-01 17:19:18 -0300

    parcialFE3

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-import Lista from './Components/Lista'
+// import Lista from './Components/Lista'
+import Card from "./Components/Card";
+
 
 function App() {
-    const [search, setSearch] = useState('')
-    const [searchList, setSearchList] = useState([])
-
-    let doctores = [
-      {id:1, nombre: 'Rick Sanchez', especialidad: 'Gastroenterologo'},
-      {id:2, nombre: 'Julius Hibbert', especialidad: 'Dermatologo'},
-      {id:3, nombre: 'Nick Riviera', especialidad: 'Otorrinonaringologo'},
-      {id:4, nombre: 'John Zoidberg', especialidad: 'Cirujano'}
-    ]
-
-    const handleSearch = () => {
-      const fillteredDocs = doctores.filter((doctor) => doctor.nombre.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
-      setSearchList(fillteredDocs)
-    }
-
-    return (
-      <>
-        <input type="text" placeholder='Busqueda &#128269;' onBlur={(event) => setSearch(event.target.value.trim())}/>
-        <button onClick={handleSearch}>&#128269;</button>
-        {searchList.length > 0 ? null : <Lista doctores={doctores}/>}
-        {search}
-
-        {searchList.length > 0 ?
-        <>
-          <h2>Doctores Encontrados</h2>
-          <Lista doctores={searchList}/>
-        </>  
-        : null
-        }
-        {(searchList.length == 0 && search.length > 0) && <h3>No se han encontrado doctores</h3>}
-  
-      </>
-    )
-  
-
+     return (
+      <div className="App">
+        <h1>Carga de estudiante</h1>
+        <form></form>
+        <Card />
+      </div>
+    );
     
   }
-
-  
 
 export default App
